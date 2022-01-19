703b73b5c0d22b33ae39c15df5f0d7812c8aff36
zhiganik <nzhehistovskyi@qualium-systems.com.ua>
2022-01-20 00:08:49 +0200

    Add dotween, some assets and rework chunk design

---
+++ Assets/Scripts/AnimationComponents/CircleMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace AnimationComponents
+{
+    public class CircleMove : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        [SerializeField] private float width;
+        [SerializeField] private float height;
+
+        [SerializeField] private GameObject[] objects;
+        
+
+        private float _timeCounter = 0;
+        
+        private void Update()
+        {
+            DoCircleMove();
+        }
+
+        private void DoCircleMove()
+        {
+            //var targetObject = objects[Random.Range(0, objects.Length)];
+            var targetTransform = transform;
+            
+            _timeCounter += Time.deltaTime;
+
+            var x = Mathf.Cos(_timeCounter) * width;
+            var y = Mathf.Sin(_timeCounter) * height;
+
+            targetTransform.position = new Vector3(x, y, targetTransform.position.z);
+            targetTransform.rotation = Quaternion.Euler(x, y, targetTransform.rotation.z);
+        }
+    }
+}
